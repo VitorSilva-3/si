e6efbf76a3a0e0cb25f198928014a72dcf465b8d
VitorSilva-3 <vtsilva3@gmail.com>
2025-09-22 11:54:31 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+__author__ = "Vítor Silva"
+__credits__ = ["Vítor Silva"]
+__license__ = "Apache License 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Vítor Silva"
+__email__ = "vtsilva3@gmail.com"
+
+# -----------------------------------------------------------------------------------
+# This package is heavily inspired and adapted from
+# - https://github.com/vmspereira/si
+# - https://github.com/cruz-f/si
+# - https://github.com/jcorreia11/si
+# -----------------------------------------------------------------------------------
