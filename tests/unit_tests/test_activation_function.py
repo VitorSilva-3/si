d73907c4484bfd3fc9c60d3a6f771ed7a1c16ce8
VitorSilva-3 <vtsilva3@gmail.com>
2026-01-14 19:08:52 +0000

    [ADD] tanh and softmax tests

--- tests/unit_tests/test_activation_function.py
+++ tests/unit_tests/test_activation_function.py
@@ -3,10 +3,11 @@ from unittest import TestCase
 from datasets import DATASETS_PATH
 
 import os
+import numpy as np
 
 from si.io.data_file import read_data_file
 from si.model_selection.split import train_test_split
-from si.neural_networks.activation import ReLUActivation, SigmoidActivation
+from si.neural_networks.activation import ReLUActivation, SigmoidActivation, TanhActivation, SoftmaxActivation
 
 class TestSigmoidLayer(TestCase):
 
@@ -52,5 +53,56 @@ class TestRELULayer(TestCase):
     def test_derivative(self):
         sigmoid_layer = ReLUActivation()
         derivative = sigmoid_layer.derivative(self.dataset.X)
+        self.assertEqual(derivative.shape[0], self.dataset.X.shape[0])
+        self.assertEqual(derivative.shape[1], self.dataset.X.shape[1])
+
+
+class TestTanhLayer(TestCase):
+
+    def setUp(self):
+        self.csv_file = os.path.join(DATASETS_PATH, 'breast_bin', 'breast-bin.csv')
+        self.dataset = read_data_file(filename=self.csv_file, label=True, sep=",")
+        self.train_dataset, self.test_dataset = train_test_split(self.dataset)
+
+    def test_activation_function(self):
+        tanh_layer = TanhActivation()
+        result = tanh_layer.activation_function(self.dataset.X)
+        
+        # Tanh compresses values to the range [-1, 1]
+        self.assertTrue(all([i >= -1 and i <= 1 for j in range(result.shape[1]) for i in result[:, j]]))
+
+    def test_derivative(self):
+        tanh_layer = TanhActivation()
+        derivative = tanh_layer.derivative(self.dataset.X)
+        
+        # Check if the derivative shape matches the input shape
+        self.assertEqual(derivative.shape[0], self.dataset.X.shape[0])
+        self.assertEqual(derivative.shape[1], self.dataset.X.shape[1])
+
+
+class TestSoftmaxLayer(TestCase):
+
+    def setUp(self):
+        self.csv_file = os.path.join(DATASETS_PATH, 'breast_bin', 'breast-bin.csv')
+        self.dataset = read_data_file(filename=self.csv_file, label=True, sep=",")
+        self.train_dataset, self.test_dataset = train_test_split(self.dataset)
+
+    def test_activation_function(self):
+        softmax_layer = SoftmaxActivation()
+        result = softmax_layer.activation_function(self.dataset.X)
+        
+        # Check if all values are between 0 and 1 (probabilities)
+        self.assertTrue(all([i >= 0 and i <= 1 for j in range(result.shape[1]) for i in result[:, j]]))
+        
+        # Check if the sum of probabilities for each sample (row) is equal to 1
+        row_sums = np.sum(result, axis=1)
+        expected_sums = np.ones(result.shape[0])
+        self.assertTrue(np.allclose(row_sums, expected_sums))
+
+    def test_derivative(self):
+        softmax_layer = SoftmaxActivation()
+        derivative = softmax_layer.derivative(self.dataset.X)
+        
+        # Check if the derivative shape matches the input shape
         self.assertEqual(derivative.shape[0], self.dataset.X.shape[0])
         self.assertEqual(derivative.shape[1], self.dataset.X.shape[1])
